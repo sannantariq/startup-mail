d07c672c0af7cec431bf4bcd8d3bacaa390dbd7d
Sannan Tariq <stariq@andrew.cmu.edu>
2017-01-22 19:36:35 +0100

    fixed rc.local by adding sleep, and removed extra ip line from code

--- startup_mailer.py
+++ startup_mailer.py
@@ -24,7 +24,7 @@ def connect_type(word_list):
 
 # Change to your own account information
 # Account Information
-to = 'rpinsl2050@email.com' # Email to send to.
+to = 'rpinsl2050@gmail.com' # Email to send to.
 gmail_user = 'rpinsl2050@gmail.com' # Email to send from. (MUST BE GMAIL)
 gmail_password = 'nsl2050edison' # Gmail password.
 smtpserver = smtplib.SMTP('smtp.gmail.com', 587) # Server to use.
@@ -43,23 +43,24 @@ data = p.communicate()  # Get data from 'p terminal'.
 # Split IP text block into three, and divide the two containing IPs into words.
 ip_lines = data[0].splitlines()
 split_line_a = ip_lines[1].split()
-split_line_b = ip_lines[2].split()
+if len(ip_lines) > 2: 
+	split_line_b = ip_lines[2].split()
 
 # con_type variables for the message text. ex) 'ethernet', 'wifi', etc.
 ip_type_a = connect_type(split_line_a)
-ip_type_b = connect_type(split_line_b)
+#ip_type_b = connect_type(split_line_b)
 
 """Because the text 'src' is always followed by an ip address,
 we can use the 'index' function to find 'src' and add one to
 get the index position of our ip.
 """
 ipaddr_a = split_line_a[split_line_a.index('src')+1]
-ipaddr_b = split_line_b[split_line_b.index('src')+1]
+#ipaddr_b = split_line_b[split_line_b.index('src')+1]
 
 # Creates a sentence for each ip address.
 my_ip_a = 'Your %s ip is %s' % (ip_type_a, ipaddr_a)
-my_ip_b = 'Your %s ip is %s' % (ip_type_b, ipaddr_b)
-
+#my_ip_b = 'Your %s ip is %s' % (ip_type_b, ipaddr_b)
+my_ip_b = "EXTRA_IP_HERE"
 # Creates the text, subject, 'from', and 'to' of the message.
 msg = MIMEText(my_ip_a + "\n" + my_ip_b)
 msg['Subject'] = 'IPs For rpi-worker-0 on %s' % today.strftime('%b %d %Y')
